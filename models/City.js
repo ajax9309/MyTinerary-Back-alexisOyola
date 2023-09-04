11a13ee7372b4d1ece7944d0da9745179c592674
ajax9309 <68715088+ajax9309@users.noreply.github.com>
2023-09-04 17:51:39 -0300

    sprint3

--- models/City.js
+++ models/City.js
@@ -1,12 +1,11 @@
 import { Schema,model } from "mongoose";
 
 const citySchema= Schema({
-    city: {type: String, required:true},
+    name: {type: String, required:true},
     country: {type: String, required:true},
     language: {type: String, required:true},
     currency: {type: String, required:true},
     image: {type: String, required:true},
-    population: {type: Number, required:true},
     description: {type: String, required:true}
 },{
     timestamps:true
